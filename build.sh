9692a7ba4645c5d4c173c597570bfe4667a77ce1
LukasBommes <lukas.bommes@gmx.de>
2022-07-29 21:41:48 +0200

    use cibuildwheel to build wheels

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export CIBW_PLATFORM='linux'
+#export CIBW_BUILD='cp${{ matrix.python }}-${{ matrix.platform_id }}'
+export CIBW_BUILD='cp39-manylinux_x86_64'
+export CIBW_SKIP='pp*'
+export CIBW_ARCHS='x86_64'
+#export CIBW_MANYLINUX_X86_64_IMAGE='${{ matrix.manylinux_image }}'
+export CIBW_MANYLINUX_X86_64_IMAGE='lubo1994/mv-extractor:latest'
+export CIBW_BUILD_FRONTEND='build'
+export CIBW_TEST_COMMAND='VIDEO_URL={project}/vid.mp4 python3 {project}/tests/tests.py'
+export CIBW_BUILD_VERBOSITY=1
+
+pip install cibuildwheel==2.8.1
+cibuildwheel --platform linux
