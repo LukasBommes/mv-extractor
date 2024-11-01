32330b5759e315c5c26627b1071c9857d568ff4b
Lukas Bommes <lukas.bommes@gmx.de>
2024-11-01 20:53:36 +0100

    move media server binary to root directory

--- tests/end_to_end_tests.py
+++ tests/end_to_end_tests.py
@@ -66,7 +66,7 @@ class TestEndToEnd(unittest.TestCase):
     def test_end_to_end_rtsp(self):
         with tempfile.TemporaryDirectory() as outdir:
             print("Setting up end to end test for RTSP")
-            media_server_binary = os.path.abspath(os.path.join(PROJECT_ROOT, "tools/live555MediaServer"))
+            media_server_binary = os.path.abspath(os.path.join(PROJECT_ROOT, "tests/tools/live555MediaServer"))
             rtsp_server = subprocess.Popen(media_server_binary, cwd=PROJECT_ROOT if PROJECT_ROOT else None)
             try:
                 time.sleep(1)
